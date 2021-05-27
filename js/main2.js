aad62effb6ef7a2fb39c22619e44515a233fbbf9
team46san <78849250+team46san@users.noreply.github.com>
2021-05-28 04:26:45 +0900

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,26 @@
+
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCU3mxQ5PvMtOsRi1S50ko0UK82kWJe7bo",
+    authDomain: "chatbotapp46.firebaseapp.com",
+    projectId: "chatbotapp46",
+    storageBucket: "chatbotapp46.appspot.com",
+    messagingSenderId: "449707000033",
+    appId: "1:449707000033:web:63f9b202297d507f38f7a0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
+  const db=firebase.firestore();
+  const collection = db.collection('messagees');
+
+collection.add({
+message: 'test'
+})
+.then(doc => {
+console.log(`${doc.id} added!`)
+})
+
